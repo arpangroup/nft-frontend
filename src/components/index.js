581753ae8eb1b67fd793f270ab94e9c16162bd5d
arpan jana <arpangroup1@gmail.com>
2025-05-18 01:51:00 +0530

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {default as Navbar} from './navbar/Navbar';
+export {default as NavbarV1} from './navbarV1/NavbarV1';
+export {default as NavbarV2} from './navbarV2/NavbarV2';
+export {default as Header} from './header/Header';
+export {default as Bids} from './bids/Bids';
+export {default as Footer} from './footer/Footer';
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export {default as Home} from './home/Home';
+export {default as Profile} from './profile/Profile';
+export {default as Item} from './item/Item';
+export {default as Create} from './create/Create';
+export {default as Login} from './login/Login';
+export {default as Register} from './register/Register';
+export {default as Forgot} from './forgot/Forgot';
+export {default as Settings} from './setting/Settings';
